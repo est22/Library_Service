bf3ce05c8fbfeccdf64be4aa4ccbde392fd45c24
est22 <anlia.seoul@gmail.com>
2021-08-17 22:11:08 +0900

    FLASK_APP과 FLASK_ENV 환경 변수 자동 설정

--- venv/bin/myproject.sh
+++ venv/bin/myproject.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /Users/lia/venvs/myproject
-source /Users/lia/venvs/myproject/bin/activate
+cd /Users/lia/LibraryService/venv
+set FLASK_APP=app
+set FLASK_ENV=development
+source /Users/lia/LibraryService/venv/myproject/bin/activate
